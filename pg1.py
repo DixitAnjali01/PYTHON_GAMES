c6b5284cf45a8286e79fa181b00b410277111a08
Anjali Dixit <122504032+DixitAnjali01@users.noreply.github.com>
2023-04-28 21:52:36 +0530

    Add files via upload

---
+++ pg1.py
@@ -0,0 +1,14 @@
+#NUMBER GUESS GAME
+import random
+score=1
+while 1:
+    x=random.randint(1000,9999)
+    print(x)
+    y=int(input("ENTER ANY FOUR DIGIT NUMBER:"))
+    if x==y:
+        print("WELL DONE YOU GUESS IT RIGHT :)!!")
+        print("THE NUMBER OF GUESSES YOU TAKE IS :",score)
+        break
+    else:
+        score+=1
+    
