75897ae94cf647603e239ae0d49db916e576affd
Anjali Dixit <122504032+DixitAnjali01@users.noreply.github.com>
2023-04-30 19:01:54 +0530

    Update pg1.py

--- pg1.py
+++ pg1.py
@@ -3,7 +3,6 @@ import random
 score=1
 while 1:
     x=random.randint(1000,9999)
-    print(x)
     y=int(input("ENTER ANY FOUR DIGIT NUMBER:"))
     if x==y:
         print("WELL DONE YOU GUESS IT RIGHT :)!!")
